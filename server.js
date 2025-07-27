c204d26934490a81c1055489c1a6f4642ce8b86d
Ware Tiebebedigha <106900112+WTiebebedigha22@users.noreply.github.com>
2025-07-27 22:58:53 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.DB_URL).then((result)=>{
+    console.log("DB Connected Successfully!");
+}).catch((err)=>{
+    console.log(err);
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Server started on port ${PORT}`);
+})
